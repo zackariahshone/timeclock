d1be51ef693cb6fe84c0e05f492bfc8d901d8f7d
Zack Shone <zackariahshone.ta@gmail.com>
2024-10-17 22:51:58 -0500

    add build script remove home page

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
 const bodyParser = require('body-parser');
 require('./dbconnection/connection');
 
@@ -10,8 +11,13 @@ require('./dbconnection/connection');
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json())
 app.use(routes);
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, '../client', 'build'))); // Assuming your React app is in a 'client' folder
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static("../client/build"));
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
