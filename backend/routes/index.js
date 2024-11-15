a45982bcecdadb961f86d2914a81867923ca149a
Zack Shone <zackariahshone.ta@gmail.com>
2024-11-14 22:32:52 -0600

    back end currating report data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,10 @@
 const StaffRoutes = require('./staff');
 const StudentHistory = require('./studentHistory.js');
 const StudentRoutes = require('./students.js');
+const Reports = require('./reports.js')
 module.exports = [
     StaffRoutes,
     StudentRoutes,
-    StudentHistory
+    StudentHistory,
+    Reports
 ];
